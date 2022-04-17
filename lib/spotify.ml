057347afefb8ce0ac89d99ca06163d65aebf6278
Davi Suga <daviciencia1@gmail.com>
2022-04-17 11:58:57 -0300

    basic env and argv parsing

--- lib/spotify.ml
+++ lib/spotify.ml
@@ -1,2 +1,14 @@
-open Cohttp
+open Utils.String
+open Utils.Env
 
+let getArgs =
+  match Sys.argv with
+  | [| _path; "Hello" |] -> "Hello World"
+  | [| _path; "Goodbye" |] -> "Goodbye World"
+  | [| head; tail |] -> "head = " ^ head ^ " and tail =" ^ tail
+  | a -> joinArray " " a |> print_string |> fun () -> "No args here"
+
+let main =
+  match getVar "API_KEY" with
+  | Error msg -> print_string msg
+  | Ok key -> print_string key
