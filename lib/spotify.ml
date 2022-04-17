0997cd9f6fe6367dc4ab77f90482a6b9cebeb6a4
Davi Suga <daviciencia1@gmail.com>
2022-04-17 15:02:34 -0300

    removed unused code

--- lib/spotify.ml
+++ lib/spotify.ml
@@ -1,12 +1,4 @@
-open Utils.String
 open Utils.Env
-
-let getArgs =
-  match Sys.argv with
-  | [| _path; "Hello" |] -> "Hello World"
-  | [| _path; "Goodbye" |] -> "Goodbye World"
-  | [| head; tail |] -> "head = " ^ head ^ " and tail =" ^ tail
-  | a -> joinArray " " a |> print_string |> fun () -> "No args here"
 
 let main =
   match getVar "API_KEY" with
