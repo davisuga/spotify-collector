ae3cb0cf2814b08b7cd6a6d28d64c482408c5a69
Davi Suga <daviciencia1@gmail.com>
2022-04-19 16:33:25 -0300

    created function to fetch the tops from spotify

--- lib/http.ml
+++ lib/http.ml
@@ -1,14 +1,37 @@
-(* open Cohttp *)
-module Spotify = struct
-  let base_url_raw = "https://api.spotify.com/v1"
-  let base_url = Uri.of_string base_url_raw
+open Cohttp_lwt_unix
+open Lwt
 
-  let getTops ?(limit = 20) ?(time_range = "short_term") ?(offset = 5) =
-    let withPath = Uri.with_path base_url "/me/top/tracks" in
-    Uri.add_query_params' withPath
+module Spotify = struct
+  let base_url = "https://api.spotify.com/v1"
+
+  type time_range = Short_term | Medium_term | Long_term
+  type top_type = Tracks | Artists | Albums
+
+  let string_of_time_range = function
+    | Short_term -> "short_term"
+    | Medium_term -> "medium_term"
+    | Long_term -> "long_term"
+
+  let string_of_top_type = function
+    | Tracks -> "tracks"
+    | Artists -> "artists"
+    | Albums -> "albums"
+
+  let get spotify_token path params =
+    let headers =
+      Cohttp.Header.of_list [ ("Authorization", "Bearer " ^ spotify_token) ]
+    in
+    let withPath = Uri.of_string (base_url ^ path) in
+    Uri.add_query_params' withPath params |> Client.get ~headers
+
+  let getTops ?(limit = 100) ?(time_range = Short_term) ?(offset = 0)
+      ?(top_type = Tracks) token =
+    get token
+      ("/me/top/" ^ string_of_top_type top_type)
       [
         ("limit", string_of_int limit);
-        ("time_range", time_range);
+        ("time_range", string_of_time_range time_range);
         ("offset", string_of_int offset);
       ]
+    >>= fun (_res, body) -> Cohttp_lwt.Body.to_string body
 end
