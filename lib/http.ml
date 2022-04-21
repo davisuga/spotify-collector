09d4371712c871b3f6e8e6b28cfb20c256389118
Davi Suga <daviciencia1@gmail.com>
2022-04-21 15:25:35 -0300

    fixed data fetching and added printing

--- lib/http.ml
+++ lib/http.ml
@@ -1,28 +1,39 @@
 open Cohttp_lwt_unix
 open Lwt
 
+let tap fn x =
+  fn x;
+  x
+
+let show_response resp body =
+  let code = resp |> Response.status |> Cohttp.Code.code_of_status in
+  Printf.printf "Response code: %d\n" code;
+  Printf.printf "Headers: %s\n"
+    (resp |> Response.headers |> Cohttp.Header.to_string);
+  body |> Cohttp_lwt.Body.to_string >|= fun body ->
+  Printf.printf "Body of length: %d\n" (String.length body)
+
 module Spotify = struct
   let base_url = "https://api.spotify.com/v1"
 
   type time_range = Short_term | Medium_term | Long_term
-  type top_type = Tracks | Artists | Albums
+  type top_type = Tracks | Artists
 
   let string_of_time_range = function
     | Short_term -> "short_term"
     | Medium_term -> "medium_term"
     | Long_term -> "long_term"
 
-  let string_of_top_type = function
-    | Tracks -> "tracks"
-    | Artists -> "artists"
-    | Albums -> "albums"
+  let string_of_top_type = function Tracks -> "tracks" | Artists -> "artists"
 
   let get spotify_token path params =
     let headers =
       Cohttp.Header.of_list [ ("Authorization", "Bearer " ^ spotify_token) ]
     in
     let withPath = Uri.of_string (base_url ^ path) in
-    Uri.add_query_params' withPath params |> Client.get ~headers
+    Uri.add_query_params' withPath params
+    |> Client.get ~headers
+    |> tap (fun _ -> print_endline ("url: " ^ base_url ^ path))
 
   let getTops ?(limit = 100) ?(time_range = Short_term) ?(offset = 0)
       ?(top_type = Tracks) token =
@@ -33,5 +44,9 @@ module Spotify = struct
         ("time_range", string_of_time_range time_range);
         ("offset", string_of_int offset);
       ]
-    >>= fun (_res, body) -> Cohttp_lwt.Body.to_string body
+    >>= fun (res, body) ->
+    let _ = show_response res body in
+    Cohttp_lwt.Body.to_string body
+
+  let save_data data = data
 end
--- lib/spotify.ml
+++ lib/spotify.ml
@@ -1,7 +1,12 @@
 open Utils.Env
-open Http
+open Http.Spotify
+open Lwt
 
 let main =
+  let categories = [ Tracks; Artists ] in
   match getVar "TOKEN" with
   | Error msg -> print_string msg
-  | Ok key -> Spotify.getTops key |> Lwt_main.run |> print_string
+  | Ok key ->
+      categories
+      |> List.map (fun top_type -> getTops ~top_type key >|= print_string)
+      |> Lwt.join |> Lwt_main.run |> ignore
